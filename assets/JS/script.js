c4b626274923caa7d175fd60d20c91ff91a505d5
Aleksandr <keytolife94@gmail.com>
2024-06-06 19:44:21 -0500

    Added localstorage, update fetchPokemons, add random pokemon

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,44 @@
-const pokeApi = "https://pokeapi.co/api/v2/pokemon/pikachu"
 const dadJokeApi = "https://icanhazdadjoke.com/"
+let dataPokemons;
 
-fetch (pokeApi)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log(data)
-    })
+// get pokemon array user
+function getPokemonsFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("userPokemons"))
+}
+
+// save pokemons array for user
+function setPokemonsToLocalStorage(pokemonList) {
+    localStorage.setItem(`userPokemons`, JSON.stringify(pokemonList));
+}
+
+function fetchRandomPokemon() {
+    // random 1302 pokemons have in api
+    const randomPokemon = Math.floor(Math.random() * 1302)
+    const pokeApi = `https://pokeapi.co/api/v2/pokemon?limit=1500`
+
+    fetch(pokeApi)
+        .then(function (response) {
+            // get results about all pokemons
+            return response.json()
+        })
+        .then(function (data) {
+            fetchPokemonByName(data.results[randomPokemon].name)
+            dataPokemons = data;
+            console.log(data)
+        })
+}
+
+function fetchPokemonByName(pokemon) {
+    const pokeApi = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    fetch(pokeApi)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(`DATAPOKEMONS`)
+            console.log(dataPokemons)
+        })
+}
 
 fetch("https://icanhazdadjoke.com/", {
     method: 'GET', //GET is the default.
@@ -16,11 +47,14 @@ fetch("https://icanhazdadjoke.com/", {
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-        }// manual, *follow, error
-    })
+    }// manual, *follow, error
+})
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
     });
+
+
+fetchRandomPokemon();
